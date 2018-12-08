0f3bca6a38f7b1a32ac2566ef1b7ffa0dcc1be7d
Manikanta Jayanth <jayanth_manikanta@yahoo.com>
2018-12-08 02:14:15 -0600

    Added POST route for creating customers

--- routes/customers.js
+++ routes/customers.js
@@ -14,4 +14,32 @@ module.exports = server => {
       return next(new errors.InvalidContentError(err));
     }
   });
+
+  // Add customers
+  server.post("/customers", async (req, res, next) => {
+    // Check whether we are passing JSON
+    if (!req.is("application/json")) {
+      return next(
+        new errors.InvalidContentError("Expects an 'application/json' value")
+      );
+    }
+
+    // Objects for customer creation
+    const { name, email, balance } = req.body;
+
+    // Create an object of Customer model
+    const customer = new Customer({
+      name,
+      email,
+      balance
+    });
+
+    try {
+      const newCustomer = await customer.save();
+      res.send(201);
+      next();
+    } catch (err) {
+      return next(new errors.InternalError(err.message));
+    }
+  });
 };
